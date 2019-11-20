b44e4ca9e62a2861c70d62a5ded6bd88a3a365b1
chrwahl <chrwahl@gmail.com>
2019-11-20 12:17:04 +0100

    update

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ self.addEventListener('install', function(event) {
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
 				'/',
-				'/index.html'
+				'/index.html',
+				'/style.css'
 			]);
 		})
 	);
